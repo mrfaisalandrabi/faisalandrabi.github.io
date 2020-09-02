85f96f043f1fceb82ad20db7aaeee0544d6f210a
faisalandrabi <68887921+faisalandrabi@users.noreply.github.com>
2020-09-02 11:38:16 -0700

    python all

---
+++ expon.py
@@ -0,0 +1,5 @@
+def expo(x,y):
+    for i in range(y-1):
+        x=x*x
+        print(x)
+    return x
--- funco.py
+++ funco.py
@@ -1,8 +1,4 @@
-def expo(x,y):
-    for i in range(y-1):
-        x=x*x
-        print(x)
-    return x
+from expon import expo
 x=10
 y=3
 print(f"{x} raised to {y} is {expo(x,y)}") 
